314c77b78b84a9125b3c694767aa275141818a5b
satoshi uotani <s.uotani.zetakansu@gmail.com>
2020-07-15 16:10:16 +0900

    add model & migration

---
+++ database/migrations/2020_07_15_070645_create_shots_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shots', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("owner_name");
+            $table->integer("age");
+            $table->string("address");
+            $table->string("phone_number");
+            $table->text("description");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shots');
+    }
+}
