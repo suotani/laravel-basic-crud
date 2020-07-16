716af329cb50af7f168c9f8d72a9e272f66b7fca
satoshi uotani <s.uotani.zetakansu@gmail.com>
2020-07-16 20:28:50 +0900

    add alert color to edit view

--- resources/views/shops/edit.blade.php
+++ resources/views/shops/edit.blade.php
@@ -9,32 +9,38 @@
     @endforeach
   </ul>
 @endif
+<?php
+function add_class($errors, $name)
+{
+  return count($errors->get($name)) > 0 ? "alert" : "";
+}
+?>
 <form action="/shop/{{$shop->id}}" method="post">
   @csrf
   @method("PATCH")
   <div class="form-group">
     <label for="exampleInputEmail1">Name</label>
-    <input type="text" name="name" class="form-control" value="{{old('name',$shop->name)}}">
+    <input type="text" name="name" class="form-control <?php echo add_class($errors, 'name') ?>" value="{{old('name',$shop->name)}}">
   </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Owner</label>
-    <input type="text" name="owner_name" class="form-control" value="{{old('owner_name', $shop->owner_name)}}">
+    <input type="text" name="owner_name" class="form-control <?php echo add_class($errors, 'owner_name') ?>" value="{{old('owner_name', $shop->owner_name)}}">
   </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Age</label>
-    <input type="number" name="age" class="form-control" value="{{old('age',$shop->age)}}">
+    <input type="number" name="age" class="form-control <?php echo add_class($errors, 'age') ?>" value="{{old('age',$shop->age)}}">
   </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Address</label>
-    <input type="text" name="address" class="form-control" value="{{old('address',$shop->address)}}">
+    <input type="text" name="address" class="form-control <?php echo add_class($errors, 'address') ?>" value="{{old('address',$shop->address)}}">
   </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Phone Number</label>
-    <input type="text" name="phone_number" class="form-control" value="{{old('phone_number', $shop->phone_number)}}">
+    <input type="text" name="phone_number" class="form-control <?php echo add_class($errors, 'phone_number') ?>" value="{{old('phone_number', $shop->phone_number)}}">
   </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Description</label>
-    <textarea name="description" class="form-control">{{old('description', $shop->description)}}</textarea>
+    <textarea name="description" class="form-control <?php echo add_class($errors, 'descrition') ?>">{{old('description', $shop->description)}}</textarea>
   </div>
   <a href="/shops" class="btn btn-secondary">Back</a>
   <button type="submit" class="btn btn-primary">Submit</button>
